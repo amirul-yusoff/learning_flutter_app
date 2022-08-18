b06f9bcf142766e391e615f5690378ba7e2f9451
amirulhafizey <amirulhafizey@gmail.com>
2022-08-19 00:10:31 +0800

    update the page backgroud

--- a_to_z_application/lib/waktu_azan_page.dart
+++ a_to_z_application/lib/waktu_azan_page.dart
@@ -14,6 +14,20 @@ class _WaktuAzanPageState extends State<WaktuAzanPage> {
       appBar: AppBar(
         title: const Text("Waktu Azan"),
       ),
+      body: Container(
+        height: 100,
+        width: 100,
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/amirul_mobile.png"),
+              fit: BoxFit.cover),
+        ),
+        child: const Text(
+          'Image in fullscreen',
+          style: TextStyle(fontSize: 34, color: Colors.red),
+        ),
+      ),
     );
   }
 }
