8fd2b55620b39726bab04978ff8d8aa9d791d8df
amirulhafizey <amirulhafizey@gmail.com>
2022-08-19 11:03:06 +0800

    my pasar app

---
+++ mypasar/android/app/src/main/kotlin/com/example/mypasar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mypasar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mypasar/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Georgia',
+          textTheme: const TextTheme(
+            headline6: TextStyle(fontSize: 20.0),
+            bodyText1: TextStyle(
+                fontSize: 12.0, fontFamily: 'Hind', color: Colors.red),
+          )),
+      title: 'My Pasar',
+      home: const Scaffold(
+        body: null,
+      ),
+    );
+  }
+}
