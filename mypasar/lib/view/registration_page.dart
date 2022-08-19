b12f1285ec285395c2c422578dd7d1aa272ee678
amirulhafizey <amirulhafizey@gmail.com>
2022-08-19 22:55:26 +0800

    update the eula

--- mypasar/lib/view/registration_page.dart
+++ mypasar/lib/view/registration_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'login_page.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({Key? key}) : super(key: key);
@@ -12,11 +17,12 @@ class _RegistrationPageState extends State<RegistrationPage> {
   bool _passwordVisible = true;
   String eula = "";
 
-  late double screenHeight, screenWidth;
+  late double screenHeight, screenWidth, resWidth;
   final focus = FocusNode();
   final focus1 = FocusNode();
   final focus2 = FocusNode();
   final focus3 = FocusNode();
+
   final TextEditingController _nameEditingController = TextEditingController();
   final TextEditingController _emailditingController = TextEditingController();
   final TextEditingController _passEditingController = TextEditingController();
@@ -27,43 +33,57 @@ class _RegistrationPageState extends State<RegistrationPage> {
   Widget build(BuildContext context) {
     screenHeight = MediaQuery.of(context).size.height;
     screenWidth = MediaQuery.of(context).size.width;
-
+    if (screenWidth <= 600) {
+      resWidth = screenWidth;
+    } else {
+      resWidth = screenWidth * 0.75;
+    }
     return Scaffold(
-      body: Stack(
-        children: [upperHalf(context), lowerHalf(context)],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [upperHalf(context), lowerHalf(context)],
+            ),
+          ),
+        ),
       ),
     );
   }
 
   Widget upperHalf(BuildContext context) {
-    return SizedBox(
-      height: screenHeight / 2,
-      width: screenWidth,
-      child: Image.asset(
-        'assets/images/login.png',
-        fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+      child: SizedBox(
+        height: screenHeight / 3.5,
+        width: resWidth * 0.7,
+        child: Image.asset(
+          'assets/images/mypasar.png',
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
 
   Widget lowerHalf(BuildContext context) {
     return Container(
-      height: 600,
-      margin: EdgeInsets.only(top: screenHeight / 5),
+      width: resWidth,
+      margin: const EdgeInsets.only(top: 20),
       padding: const EdgeInsets.only(left: 10, right: 10),
-      child: SingleChildScrollView(
-          child: Column(
+      child: Column(
         children: [
           Card(
-              elevation: 10,
+            elevation: 10,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(25, 10, 20, 25),
               child: Form(
                 key: _formKey,
                 child: Column(
                   children: <Widget>[
-                    const Text(
+                    Text(
                       "Register New Account",
                       style: TextStyle(
-                        fontSize: 24,
+                        fontSize: resWidth * 0.05,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
@@ -71,49 +91,42 @@ class _RegistrationPageState extends State<RegistrationPage> {
                       height: 10,
                     ),
                     TextFormField(
-                      textInputAction: TextInputAction.next,
-                      validator: (val) => val!.isEmpty || (val.length < 3)
-                          ? "name must be longer that 3 character"
-                          : null,
-                      onFieldSubmitted: (v) {
-                        FocusScope.of(context).requestFocus(focus);
-                      },
-                      controller: _nameEditingController,
-                      keyboardType: TextInputType.text,
-                      decoration: const InputDecoration(
-                          labelText: "Name",
-                          labelStyle: TextStyle(),
-                          icon: Icon(Icons.person),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 2.0))),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
+                        textInputAction: TextInputAction.next,
+                        validator: (val) => val!.isEmpty || (val.length < 3)
+                            ? "name must be longer than 3"
+                            : null,
+                        onFieldSubmitted: (v) {
+                          FocusScope.of(context).requestFocus(focus);
+                        },
+                        controller: _nameEditingController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                            labelText: 'Name',
+                            labelStyle: TextStyle(),
+                            icon: Icon(Icons.person),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2.0),
+                            ))),
                     TextFormField(
-                      textInputAction: TextInputAction.next,
-                      validator: (val) => val!.isEmpty ||
-                              (val.length < 3 ||
-                                  !val.contains("@") ||
-                                  !val.contains("."))
-                          ? "emter a valid email"
-                          : null,
-                      focusNode: focus,
-                      onFieldSubmitted: (v) {
-                        FocusScope.of(context).requestFocus(focus1);
-                      },
-                      controller: _emailditingController,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                          labelText: "Email",
-                          labelStyle: TextStyle(),
-                          icon: Icon(Icons.phone),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 2.0))),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
+                        textInputAction: TextInputAction.next,
+                        validator: (val) => val!.isEmpty ||
+                                !val.contains("@") ||
+                                !val.contains(".")
+                            ? "enter a valid email"
+                            : null,
+                        focusNode: focus,
+                        onFieldSubmitted: (v) {
+                          FocusScope.of(context).requestFocus(focus1);
+                        },
+                        controller: _emailditingController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            labelStyle: TextStyle(),
+                            labelText: 'Email',
+                            icon: Icon(Icons.phone),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2.0),
+                            ))),
                     TextFormField(
                       textInputAction: TextInputAction.done,
                       validator: (val) => validatePassword(val.toString()),
@@ -142,9 +155,6 @@ class _RegistrationPageState extends State<RegistrationPage> {
                             borderSide: BorderSide(width: 2.0),
                           )),
                       obscureText: _passwordVisible,
-                    ),
-                    const SizedBox(
-                      height: 10,
                     ),
                     TextFormField(
                       style: const TextStyle(),
@@ -184,7 +194,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
                       obscureText: _passwordVisible,
                     ),
                     const SizedBox(
-                      height: 10,
+                      height: 15,
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -199,7 +209,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
                         ),
                         Flexible(
                           child: GestureDetector(
-                            onTap: null,
+                            onTap: _showEULA,
                             child: const Text('Agree with terms',
                                 style: TextStyle(
                                   fontSize: 16,
@@ -207,29 +217,121 @@ class _RegistrationPageState extends State<RegistrationPage> {
                                 )),
                           ),
                         ),
-                        MaterialButton(
-                          color: Colors.red,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0)),
-                          minWidth: 115,
-                          height: 50,
-                          child: const Text("Register"),
-                          elevation: 10,
-                          onPressed: () {
-                            // Navigator.of(context).pop();
-                            _registerUserAccount();
-                          },
-                        ),
-                        const SizedBox(
-                          height: 10,
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              fixedSize: Size(screenWidth / 3, 50)),
+                          child: const Text('Register'),
+                          onPressed: _registerAccountDialog,
                         ),
                       ],
-                    )
+                    ),
                   ],
                 ),
-              ))
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Already Register? ",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  )),
+              GestureDetector(
+                onTap: () => {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => const LoginPage()))
+                },
+                child: const Text(
+                  "Login here",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          GestureDetector(
+            onTap: null,
+            child: const Text(
+              "Back to Home",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
         ],
-      )),
+      ),
+    );
+  }
+
+  void _registerAccountDialog() {
+    if (!_formKey.currentState!.validate()) {
+      Fluttertoast.showToast(
+          msg: "Please complete the registration form first",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          fontSize: 14.0);
+      return;
+    }
+    if (!_isChecked) {
+      Fluttertoast.showToast(
+          msg: "Please accept the terms and conditions",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          fontSize: 14.0);
+      return;
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          title: const Text(
+            "Register new account?",
+            style: TextStyle(),
+          ),
+          content: const Text("Are you sure?", style: TextStyle()),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                "Yes",
+                style: TextStyle(),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _registerUserAccount();
+              },
+            ),
+            TextButton(
+              child: const Text(
+                "No",
+                style: TextStyle(),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -246,6 +348,57 @@ class _RegistrationPageState extends State<RegistrationPage> {
         return null;
       }
     }
+  }
+
+  loadEula() async {
+    eula = await rootBundle.loadString('assets/eula.txt');
+  }
+
+  void _showEULA() {
+    loadEula();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            "EULA",
+          ),
+          content: SizedBox(
+            height: screenHeight / 1.5,
+            width: screenWidth,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: SingleChildScrollView(
+                      child: RichText(
+                    softWrap: true,
+                    textAlign: TextAlign.justify,
+                    text: TextSpan(
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.black,
+                        ),
+                        text: eula),
+                  )),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                "Close",
+                style: TextStyle(),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
   }
 
   void _registerUserAccount() {}
