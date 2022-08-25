0678c13db52a04d8cb2c5436853c869bc43ff816
amirul-yusoff <amirulhafizey@gmail.com>
2022-08-25 23:10:43 +0800

    1st commit

--- app_kms/lib/view/daily_record_list_by_project_page.dart
+++ app_kms/lib/view/daily_record_list_by_project_page.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_dropdown_search/flutter_dropdown_search.dart';
+import 'model/assetList.dart';
+import 'model/config.dart';
 import 'model/user.dart';
+import 'package:http/http.dart' as http;
 
 class DailyRecordByProjectPage extends StatefulWidget {
   final User user;
@@ -13,11 +18,44 @@ class DailyRecordByProjectPage extends StatefulWidget {
 }
 
 class _DailyRecordByProjectPageState extends State<DailyRecordByProjectPage> {
+  TextEditingController _controller = TextEditingController();
+  List assetlist = [];
+  String titlecenter = "Loading Asset List...";
+  List<String> items = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Daily Record By Project'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        child: Column(
+          children: [
+            FlutterDropdownSearch(
+              textController: _controller,
+              items: items,
+              dropdownHeight: 300,
+            )
+          ],
+        ),
       ),
     );
   }
